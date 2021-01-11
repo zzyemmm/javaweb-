0234c6d01c29017da827079cd0cd2fa32532521c
zzyemmm <847464807@qq.com>
2021-01-11 17:29:11 +0800

    day1

---
+++ day1/2.二维码的生成/training/exercise/day1/TestQRcode.java
@@ -0,0 +1,32 @@
+package training.exercise.day1;
+
+import cn.tedu.qr.CreateQR;
+import org.junit.Test;
+
+/**
+ * @author:zzy
+ * @email:847464807@qq.com
+ * @version:v1.1.0
+ * @time:2021.1.11
+ * @description:
+ */
+
+
+public class TestQRcode {
+    //main方法
+    public static void main(String[] args){
+        System.out.println("生成二维码");
+    }
+
+    //单元测试方法  运行时只运行单元测试里面的
+    @Test
+    public void testQR(){
+        System.out.println("单元测试方法");
+
+        String url ="https://y.qq.com/";
+        String fileName = "F:\\实训\\项目\\QQmusic.png";
+
+        CreateQR.create(200,200,url,fileName);
+        System.out.println("创建二维码成功！");
+    }
+}
